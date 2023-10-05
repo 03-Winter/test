f92c4586dc5c81327969c945c88d6e029fd8c646
03-Winter <1487262758@qq.com>
2023-10-05 19:09:08 +0800

    基本简单指针语法

---
+++ 2023五月/test 5-13/test 5-13/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//int cmp_int(const void* e1, const void * e2)
+//{
+//	return *(int*)e1 - *(int*)e2; 
+//}
+//void print(int S[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", S[i]);
+//	}
+//	
+//}
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//};
+//int sort_by_age(const void* e1, const void* e2)
+//{
+//	return ((struct stu*)e1)->age - ((struct stu*)e2)->age;
+//}
+//
+//int sort_by_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((struct stu*)e1)->name,((struct stu*)e2)->name);
+//}
+//
+//void test1()
+//{
+//	struct stu arr[3] = { {"changsan",28},{"lisi",19},{"zhongqing",52} };
+//	int es = sizeof(arr) / sizeof(arr[0]);
+//
+//	qsort(arr, es, sizeof(arr[0]), sort_by_age);
+//	qsort(arr, es, sizeof(arr[0]), sort_by_name);
+//	print(arr, es);
+//
+//void test2()
+//{
+//	int S[] = { 9,8,7,6,5,4,3,2,1,0 };
+//
+//
+//	int sz = sizeof(S) / sizeof(S[0]);
+//	qsort(S, sz, sizeof(S[0]), cmp_int);
+//	print(S, sz);
+//}
+//
+//int main()
+//{
+//	test1();
+//	test2();
+//	return 0;
+//}
+
+
