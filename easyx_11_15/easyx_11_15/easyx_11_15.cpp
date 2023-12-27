6ac7397ba5fae6cbca680d3dfcc96f40a20f3b13
03-Winter <1487262758@qq.com>
2023-12-27 20:20:58 +0800

    easyx

---
+++ easyx_11_15/easyx_11_15/easyx_11_15.cpp
@@ -0,0 +1,15 @@
+﻿// easyx_11_15.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+//#include <iostream>
+#include<stdio.h>
+#include<easyx.h>
+
+
+
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+}
+
