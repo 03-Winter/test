7fae3c470ab693aeb763cb71669b2b951782e1af
03-Winter <1487262758@qq.com>
2024-01-01 13:53:16 +0800

    。

---
+++ 初次学内容杂/test_11_27/test_11_27/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS  1
+
+//#include<stdio.h>
+//void reverse(int n)
+//{
+//    if (n == 0) return 0;
+//    else {
+//        printf("%d", n % 10);
+//        return reverse(n / 10);
+//    }
+//}
+//
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    reverse(n);
+//    return 0;
+//}
